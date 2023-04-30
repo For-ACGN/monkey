ffc0bfc0af8971accf90e496c59e5d49c779f91d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2023-04-30 14:40:26 +0800

    improve code style about return value and variable, add #nosec for unsafe.

--- jmp_amd64.go
+++ jmp_amd64.go
@@ -1,6 +1,6 @@
 package monkey
 
-func buildJmpDirective(double uintptr) []byte {
+func buildJMPDirective(double uintptr) []byte {
 	d0 := byte(double)
 	d1 := byte(double >> 8)
 	d2 := byte(double >> 16)
@@ -9,9 +9,9 @@ func buildJmpDirective(double uintptr) []byte {
 	d5 := byte(double >> 40)
 	d6 := byte(double >> 48)
 	d7 := byte(double >> 56)
-
-	return []byte{
+	jmp := []byte{
 		0x48, 0xBA, d0, d1, d2, d3, d4, d5, d6, d7, // MOV rdx, double
 		0xFF, 0x22, // JMP [rdx]
 	}
+	return jmp
 }
--- jmp_arm64.go
+++ jmp_arm64.go
@@ -2,33 +2,29 @@ package monkey
 
 import "unsafe"
 
-func buildJmpDirective(double uintptr) []byte {
-	res := make([]byte, 0, 24)
+func buildJMPDirective(double uintptr) []byte {
 	d0d1 := double & 0xFFFF
 	d2d3 := double >> 16 & 0xFFFF
 	d4d5 := double >> 32 & 0xFFFF
 	d6d7 := double >> 48 & 0xFFFF
-
-	res = append(res, movImm(0b10, 0, d0d1)...)          // MOVZ x26, double[16:0]
-	res = append(res, movImm(0b11, 1, d2d3)...)          // MOVK x26, double[32:16]
-	res = append(res, movImm(0b11, 2, d4d5)...)          // MOVK x26, double[48:32]
-	res = append(res, movImm(0b11, 3, d6d7)...)          // MOVK x26, double[64:48]
-	res = append(res, []byte{0x4A, 0x03, 0x40, 0xF9}...) // LDR x10, [x26]
-	res = append(res, []byte{0x40, 0x01, 0x1F, 0xD6}...) // BR x10
-
-	return res
+	jmp := make([]byte, 0, 24)
+	jmp = append(jmp, movImm(0x02, 0, d0d1)...)          // MOVZ x26, double[16:0]
+	jmp = append(jmp, movImm(0x03, 1, d2d3)...)          // MOVK x26, double[32:16]
+	jmp = append(jmp, movImm(0x03, 2, d4d5)...)          // MOVK x26, double[48:32]
+	jmp = append(jmp, movImm(0x03, 3, d6d7)...)          // MOVK x26, double[64:48]
+	jmp = append(jmp, []byte{0x4A, 0x03, 0x40, 0xF9}...) // LDR x10, [x26]
+	jmp = append(jmp, []byte{0x40, 0x01, 0x1F, 0xD6}...) // BR x10
+	return jmp
 }
 
 func movImm(opc, shift int, val uintptr) []byte {
-	var m uint32 = 26          // rd
-	m |= uint32(val) << 5      // imm16
-	m |= uint32(shift&3) << 21 // hw
-	m |= 0b100101 << 23        // const
-	m |= uint32(opc&0x3) << 29 // opc
-	m |= 0b1 << 31             // sf
-
-	res := make([]byte, 4)
-	*(*uint32)(unsafe.Pointer(&res[0])) = m
-
-	return res
+	var m uint32 = 26           // rd
+	m |= uint32(val) << 5       // imm16
+	m |= uint32(shift&3) << 21  // hw
+	m |= 0x25 << 23             // const
+	m |= uint32(opc&0x03) << 29 // opc
+	m |= 0x01 << 31             // sf
+	b := make([]byte, 4)
+	*(*uint32)(unsafe.Pointer(&b[0])) = m // #nosec
+	return b
 }
