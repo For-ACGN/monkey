f7f145f0b25dddcaa424da7fdc888c81cc18c9a0
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2023-04-30 15:25:03 +0800

    improve check linux shell script, check windows, linux and darwin OS.

--- check.sh
+++ check.sh
@@ -1,4 +1,33 @@
-golint -set_exit_status -min_confidence 0.3 ./...
-gocyclo -avg -over 15 .
-golangci-lint run ./...
-gosec -quiet ./...
+# go1.20
+
+function check() {
+  golint -set_exit_status -min_confidence 0.3 ./...
+  gocyclo -avg -over 15 .
+  golangci-lint run ./...
+  gosec -quiet ./...
+}
+
+# linux
+export GOOS=linux
+export GOARCH=amd64
+check
+export GOARCH=386
+check
+export GOARCH=arm64
+check
+
+# windows
+export GOOS=windows
+export GOARCH=amd64
+check
+export GOARCH=386
+check
+export GOARCH=arm64
+check
+
+# darwin
+export GOOS=darwin
+export GOARCH=amd64
+check
+export GOARCH=arm64
+check
