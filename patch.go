6adedfb8eacbc9feae807862d4d3b597d4a28a22
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-02-10 20:57:32 +0800

    improve PatchGuard.patchFunc, refactor Unpatch and Restore, update checkFunc.

--- patch.go
+++ patch.go
@@ -9,21 +9,18 @@ import (
 // PatchGuard contains original and patch data for unpatch.
 type PatchGuard struct {
 	original []byte
+	target   reflect.Value
 	patch    reflect.Value
-	value    reflect.Value
 }
 
 func (pg *PatchGuard) patchFunc(target, patch reflect.Value) {
 	pg.checkFunc(target, patch)
-	assTarget := *(*uintptr)(getPointer(target))
-	pg.original = replace(assTarget, uintptr(getPointer(patch)))
-	pg.patch = patch
+	tAddr := *(*uintptr)(getPointer(target))
+	pAddr := uintptr(getPointer(patch))
+	pg.original = replaceFunc(tAddr, pAddr)
 }
 
 func (pg *PatchGuard) checkFunc(target, patch reflect.Value) {
-	if target.Kind() != reflect.Func {
-		panic("target is not a function")
-	}
 	if patch.Kind() != reflect.Func {
 		panic("patch is not a function")
 	}
@@ -32,7 +29,7 @@ func (pg *PatchGuard) checkFunc(target, patch reflect.Value) {
 	patchType := patch.Type()
 
 	// check the number of the function parameter and return value are equal
-	invalidIn := targetType.NumIn() < patchType.NumIn()
+	invalidIn := targetType.NumIn() != patchType.NumIn()
 	invalidOut := targetType.NumOut() != patchType.NumOut()
 	invalidVar := targetType.NumIn() == patchType.NumIn() &&
 		targetType.IsVariadic() != patchType.IsVariadic()
@@ -64,16 +61,27 @@ func (pg *PatchGuard) checkFunc(target, patch reflect.Value) {
 	}
 }
 
-func replace(target, patch uintptr) []byte {
-	code := buildJMPDirective(patch)
-	bytes := entryAddress(target, len(code))
-	original := make([]byte, len(bytes))
-	copy(original, bytes)
-	modifyBinary(target, code)
+func replaceFunc(target, patch uintptr) []byte {
+	jmp := buildJMPDirective(patch)
+	data := readMemory(target, len(jmp))
+	original := make([]byte, len(data))
+	copy(original, data)
+	modifyBinary(target, jmp)
 	return original
 }
 
-func entryAddress(p uintptr, l int) []byte {
+// Unpatch is used to recovery the original about target.
+func (pg *PatchGuard) Unpatch() {
+	address := *(*uintptr)(getPointer(pg.target))
+	modifyBinary(address, pg.original)
+}
+
+// Restore is used to patch the target again.
+func (pg *PatchGuard) Restore() {
+	pg.patchFunc(pg.target, pg.patch)
+}
+
+func readMemory(p uintptr, l int) []byte {
 	var b []byte
 	sh := (*reflect.SliceHeader)(unsafe.Pointer(&b)) // #nosec
 	sh.Data = p
@@ -82,11 +90,11 @@ func entryAddress(p uintptr, l int) []byte {
 	return b
 }
 
-type funcValue struct {
-	_ uintptr
-	p unsafe.Pointer
+type reflectValue struct {
+	_   uintptr
+	ptr unsafe.Pointer
 }
 
 func getPointer(v reflect.Value) unsafe.Pointer {
-	return (*funcValue)(unsafe.Pointer(&v)).p // #nosec
+	return (*reflectValue)(unsafe.Pointer(&v)).ptr // #nosec
 }
