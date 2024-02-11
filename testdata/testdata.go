2d5659c707e24ae5db30896a0a7b48476dbdcdd3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-02-11 20:35:21 +0800

    add testdata package, it contains structures for test patch methods.

---
+++ testdata/testdata.go
@@ -0,0 +1,28 @@
+package testdata
+
+import (
+	"fmt"
+)
+
+type Writer struct {
+	data []byte
+}
+
+func (w *Writer) Write(b []byte) (int, error) {
+	w.data = b
+	return w.print()
+}
+
+func (w *Writer) Print() (int, error) {
+	return w.print()
+}
+
+func (w Writer) Println() (int, error) {
+	n, err := w.print()
+	fmt.Println()
+	return n + 1, err
+}
+
+func (w *Writer) print() (int, error) {
+	return fmt.Println(string(w.data))
+}
