735beeec0e502562ad369e95511e1f8ec1a0839a
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-02-11 20:34:36 +0800

    add TestPatchMethod, rename TestFunction to TestPatch.

--- monkey_test.go
+++ monkey_test.go
@@ -5,9 +5,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+
+	"github.com/For-ACGN/monkey/testdata"
 )
 
-func TestFunction(t *testing.T) {
+func TestPatch(t *testing.T) {
 	output := fmt.Sprintln("hello!")
 	require.Equal(t, "hello!\n", output)
 
@@ -33,4 +35,51 @@ func TestFunction(t *testing.T) {
 	require.Equal(t, "what!!!\n", output)
 	output = fmt.Sprintln("??????")
 	require.Equal(t, "what!!!\n", output)
+}
+
+func TestPatchMethod(t *testing.T) {
+	w := new(testdata.Writer)
+
+	t.Run("interface function", func(t *testing.T) {
+		n, err := w.Write([]byte("hello!"))
+		require.NoError(t, err)
+		require.Equal(t, 7, n)
+
+		patch := func(*testdata.Writer, []byte) (int, error) {
+			return 0, nil
+		}
+		pg := PatchMethod(w, "Write", patch)
+
+		n, err = w.Write([]byte("hello!"))
+		require.NoError(t, err)
+		require.Zero(t, n)
+
+		pg.Unpatch()
+		n, err = w.Write([]byte("hello!"))
+		require.NoError(t, err)
+		require.Equal(t, 7, n)
+	})
+
+	t.Run("common function", func(t *testing.T) {
+		n, err := w.Write([]byte("hello!"))
+		require.NoError(t, err)
+		require.Equal(t, 7, n)
+		n, err = w.Print()
+		require.NoError(t, err)
+		require.Equal(t, 7, n)
+
+		patch := func(*testdata.Writer) (int, error) {
+			return fmt.Println("oh!")
+		}
+		pg := PatchMethod(w, "Print", patch)
+
+		n, err = w.Print()
+		require.NoError(t, err)
+		require.Equal(t, 4, n)
+
+		pg.Unpatch()
+		n, err = w.Print()
+		require.NoError(t, err)
+		require.Equal(t, 7, n)
+	})
 }
