8c62d584278f03d9d38014e052c5461529708993
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-02-14 20:46:35 +0800

    update Writer.Print and Println methods, change Writer.println().

--- testdata/testdata.go
+++ testdata/testdata.go
@@ -14,17 +14,19 @@ func (w *Writer) Write(b []byte) (int, error) {
 }
 
 func (w *Writer) Print() (int, error) {
-	return w.print()
-}
-
-func (w Writer) Println() (int, error) {
 	n, err := w.print()
 	fmt.Println()
 	return n + 1, err
 }
 
+func (w Writer) Println() (int, error) {
+	n, err := w.println()
+	fmt.Println()
+	return n, err
+}
+
 func (w *Writer) print() (int, error) {
-	return fmt.Println(string(w.data))
+	return fmt.Print(string(w.data) + "\n")
 }
 
 func (w Writer) println() (int, error) {
