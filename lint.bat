016c8147c8b6a0edd5709756566e15ebf30998f9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-02-19 20:33:37 +0800

    move old check code to new check_all function, update main.

--- lint.bat
+++ lint.bat
@@ -21,7 +21,10 @@ exit /b %exit_code%
     set exit_on_error=1
   )
 
+  rem start check code
+  call :check_all %1
 
+  rem check exit code
   if %exit_code% == 0 (
     echo all check passed
   ) else (
@@ -30,113 +33,22 @@ exit /b %exit_code%
 goto :EOF
 rem END_main
 
-rem -----------------Windows-----------------
-set GOOS=windows
-rem -----------------------------
-set GOARCH=amd64
-call :check %1
-rem --------exit on error--------
-if not exit_code == 0 (
-  if %exit_on_error% == 1 (
-    goto :exit_bat
-  )
-)
-rem -----------------------------
-set GOARCH=386
-call :check %1
-rem --------exit on error--------
-if not exit_code == 0 (
-  if %exit_on_error% == 1 (
-    goto :exit_bat
-  )
-)
-rem -----------------------------
-set GOARCH=arm64
-call :check %1
-rem --------exit on error--------
-if not exit_code == 0 (
-  if %exit_on_error% == 1 (
-    goto :exit_bat
-  )
-)
-rem -----------------------------
-
-
-rem ------------------Linux------------------
-set GOOS=linux
-rem -----------------------------
-set GOARCH=amd64
-call :check %1
-rem --------exit on error--------
-if not exit_code == 0 (
-  if %exit_on_error% == 1 (
-    goto :exit_bat
-  )
-)
-rem -----------------------------
-set GOARCH=386
-call :check %1
-rem --------exit on error--------
-if not exit_code == 0 (
-  if %exit_on_error% == 1 (
-    goto :exit_bat
-  )
-)
-rem -----------------------------
-set GOARCH=arm64
-call :check %1
-rem --------exit on error--------
-if not exit_code == 0 (
-  if %exit_on_error% == 1 (
-    goto :exit_bat
-  )
-)
-rem -----------------------------
-set GOARCH=loong64
-call :check %1
-rem --------exit on error--------
-if not exit_code == 0 (
-  if %exit_on_error% == 1 (
-    goto :exit_bat
-  )
-)
-rem -----------------------------
-
-
-rem -----------------Darwin------------------
-set GOOS=darwin
-rem -----------------------------
-set GOARCH=amd64
-call :check %1
-rem --------exit on error--------
-if not exit_code == 0 (
-  if %exit_on_error% == 1 (
-    goto :exit_bat
-  )
-)
-rem -----------------------------
-set GOARCH=arm64
-call :check %1
-rem --------exit on error--------
-if not exit_code == 0 (
-  if %exit_on_error% == 1 (
-    goto :exit_bat
-  )
-)
-rem -----------------------------
-
 :is_print_help
   if "%1" == "-help" (
     set is_print_help=1
+    goto :EOF
   )
   if "%1" == "--help" (
     set is_print_help=1
+    goto :EOF
   )
   if "%1" == "-h" (
     set is_print_help=1
+    goto :EOF
   )
-  if "%1" == "/?" (
+  if "%1" == "?" (
     set is_print_help=1
+    goto :EOF
   )
 goto :EOF
 rem END_is_print_help
@@ -154,6 +66,102 @@ rem END_is_print_help
   echo   "lint -gosec -e"  only use gosec and exit on error
 goto :EOF
 rem END_print_help
+
+:check_all
+  rem -----------------Windows-----------------
+  set GOOS=windows
+  rem -----------------------------
+  set GOARCH=amd64
+  call :check %1
+  rem --------exit on error--------
+  if not exit_code == 0 (
+    if %exit_on_error% == 1 (
+      goto :EOF
+    )
+  )
+  rem -----------------------------
+  set GOARCH=386
+  call :check %1
+  rem --------exit on error--------
+  if not exit_code == 0 (
+    if %exit_on_error% == 1 (
+      goto :EOF
+    )
+  )
+  rem -----------------------------
+  set GOARCH=arm64
+  call :check %1
+  rem --------exit on error--------
+  if not exit_code == 0 (
+    if %exit_on_error% == 1 (
+      goto :EOF
+    )
+  )
+  rem -----------------------------------------
+
+  rem ------------------Linux------------------
+  set GOOS=linux
+  rem -----------------------------
+  set GOARCH=amd64
+  call :check %1
+  rem --------exit on error--------
+  if not exit_code == 0 (
+    if %exit_on_error% == 1 (
+      goto :EOF
+    )
+  )
+  rem -----------------------------
+  set GOARCH=386
+  call :check %1
+  rem --------exit on error--------
+  if not exit_code == 0 (
+    if %exit_on_error% == 1 (
+      goto :EOF
+    )
+  )
+  rem -----------------------------
+  set GOARCH=arm64
+  call :check %1
+  rem --------exit on error--------
+  if not exit_code == 0 (
+    if %exit_on_error% == 1 (
+      goto :EOF
+    )
+  )
+  rem -----------------------------
+  set GOARCH=loong64
+  call :check %1
+  rem --------exit on error--------
+  if not exit_code == 0 (
+    if %exit_on_error% == 1 (
+      goto :EOF
+    )
+  )
+  rem -----------------------------------------
+
+  rem -----------------Darwin------------------
+  set GOOS=darwin
+  rem -----------------------------
+  set GOARCH=amd64
+  call :check %1
+  rem --------exit on error--------
+  if not exit_code == 0 (
+    if %exit_on_error% == 1 (
+      goto :EOF
+    )
+  )
+  rem -----------------------------
+  set GOARCH=arm64
+  call :check %1
+  rem --------exit on error--------
+  if not exit_code == 0 (
+    if %exit_on_error% == 1 (
+      goto :EOF
+    )
+  )
+  rem -----------------------------------------
+goto :EOF
+rem END_check_all
 
 :check
   echo ================================================
@@ -215,9 +223,8 @@ rem END_print_help
   gosec -quiet ./...
   call :set_exit_code
   echo ------------------------------------------------
-
   call :echo_line
-  goto :EOF
+goto :EOF
 rem END_check
 
 :set_exit_code
@@ -226,11 +233,11 @@ rem END_check
   ) else (
     set exit_code=1
   )
-  goto :EOF
+goto :EOF
 rem END_set_exit_code
 
 :echo_line
   echo ================================================
   echo.
-  goto :EOF
+goto :EOF
 rem END_echo_line
