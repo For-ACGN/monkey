9fe36fda5bccf2327cacee314522d24e47c173de
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2024-02-20 16:59:43 +0800

    improve lint.sh from lint.bat, add print_help and check_all.

--- lint.sh
+++ lint.sh
@@ -1,8 +1,69 @@
 #!/bin/bash
 
-function main() {
+function init() {
+  # initialize environment variables
+  export is_print_help=0
   export exit_code=0
+  export exit_on_error=0
+}
 
+function main() {
+  # process print help information
+  is_print_help $1
+  if [$is_print_help == 1]
+  then
+    print_help
+    return
+  fi
+  # process arguments
+  if [$2 == -e]
+  then
+    export exit_on_error=1
+  fi
+  # start check code
+  check_all $1
+  # check exit code
+  if [$exit_code == 0]
+  then
+    echo all check passed
+  else
+    echo exit code: $exit_code
+  fi
+  return $exit_code
+}
+
+function is_print_help() {
+  if [$1 == -help]
+  then
+    export is_print_help=1
+    return
+  fi
+  if [$1 == --help]
+  then
+    export is_print_help=1
+    return
+  fi
+  if [$1 == -h]
+  then
+    export is_print_help=1
+    return
+  fi
+}
+
+function print_help() {
+  echo Usage of lint:
+  echo   -golint      only use golint to check code
+  echo   -gocyclo     only use gocyclo to check code
+  echo   -cilint      only use golangci-lint to check code
+  echo   -gosec       only use gosec to check code
+  echo   -e           interrupt script when detect error
+  echo.
+  echo example:
+  echo   "./lint.sh -golint"    only use golint to check code
+  echo   "./lint.sh -gosec -e"  only use gosec and exit on error
+}
+
+function check_all() {
   # linux
   export GOOS=linux
   export GOARCH=amd64
@@ -29,16 +90,6 @@ function main() {
   check $1
   export GOARCH=arm64
   check $1
-
-  # check exit code
-  if [$exit_code == 0]
-  then
-    echo all check passed
-  else
-    echo exit code: $exit_code
-  fi
-
-  return $exit_code
 }
 
 function check() {
@@ -51,24 +102,28 @@ function check() {
     golint -set_exit_status -min_confidence 0.3 ./...
     set_exit_code
     echo_line
+    return
   ;;
 
   -gocyclo)
     gocyclo -avg -over 15 .
     set_exit_code
     echo_line
+    return
   ;;
 
   -cilint)
     golangci-lint run ./...
     set_exit_code
     echo_line
+    return
   ;;
 
   -gosec)
     gosec -quiet ./...
     set_exit_code
     echo_line
+    return
   ;;
 
   *)
@@ -109,11 +164,11 @@ function check() {
 }
 
 function set_exit_code() {
-  if [$? != 0]
+  if [$? == 0]
   then
-    export exit_code=1
-  else
     echo pass
+  else
+    export exit_code=1
   fi
 }
 
@@ -122,4 +177,5 @@ function echo_line() {
   echo
 }
 
-main $1
+init
+main $1 $2
